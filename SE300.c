b13bcf41f39bf69a818ed0b35fee5b7a992140a6
ELAlvarado <132153888+ELAlvarado@users.noreply.github.com>
2025-11-15 20:33:37 -0700

    Update SE300.c
    Added a tiny bit of code to start with.

--- SE300.c
+++ SE300.c
@@ -7,3 +7,20 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+
+typedef struct Manager{ // Leilani's note: I don't remember at all how I'm supposed to
+                        // do this properly. Feel free to delete it if it's nonsense.
+}Manager;
+
+
+int main(){
+int balance;
+  Manager DealerManager;
+  Manager TutorialManager;
+  Manager GameManager;
+  Manager BettingManager;
+  printf("***********GAME START**********\n");
+  
+  
+  return 0;
+}
